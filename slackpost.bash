54d16b59d1e8bd830a662957b9e72ae6a8c0dff0
Gurmehar Singh <gurmehar@gmail.com>
2023-10-11 12:12:24 -0400

    change Slack message error

--- slackpost.bash
+++ slackpost.bash
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -d "text=\`TEST MESSAGE: User $1 starting observation on project $2: $2\`" -d "channel=${ATACHANNEL}" -H "Authorization: Bearer ${ATATOKEN}" -X POST https://slack.com/api/chat.postMessage
+curl -d "text=\`TEST MESSAGE: User $1 starting observation on project $2: $3\`" -d "channel=${ATACHANNEL}" -H "Authorization: Bearer ${ATATOKEN}" -X POST https://slack.com/api/chat.postMessage
 echo ""
 
 
